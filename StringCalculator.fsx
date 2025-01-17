756f114745a2324af5dfe4188f460910045497e4
CamilaCarrenoV <camila.carreno89@gmail.com>
2025-01-17 08:17:16 -0300

    Added support for custom delimiters.

--- StringCalculator.fsx
+++ StringCalculator.fsx
@@ -1,11 +1,22 @@
 let add numbers =
     match numbers with
     | "" -> 0
+    | _ when numbers.StartsWith("//") ->
+        let delimiterEndIndex = numbers.IndexOf('\n')
+        let customDelimiter = numbers.Substring(2, delimiterEndIndex - 2)
+        let numbersPart = numbers.Substring(delimiterEndIndex + 1)
+        let arrayNumbers = 
+            numbersPart.Split([| customDelimiter.[0]; ','; '\n' |])
+            |> Array.map (fun x -> 
+                match System.Int32.TryParse(x) with
+                | (true, value) -> value 
+                | (false, _) -> 0)
+        arrayNumbers |> Array.sum
     | _ -> 
-     let arrayNumbers = 
-         numbers.Split([| ','; '\n' |])
-         |> Array.map (fun x -> 
-             match System.Int32.TryParse(x) with
-             | (true, value) -> value 
-             | (false, _) -> 0)     
-     arrayNumbers |> Array.sum
+        let arrayNumbers = 
+            numbers.Split([| ','; '\n' |])
+            |> Array.map (fun x -> 
+                match System.Int32.TryParse(x) with
+                | (true, value) -> value 
+                | (false, _) -> 0)
+        arrayNumbers |> Array.sum
