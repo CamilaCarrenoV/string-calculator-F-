8cfd13360c4c9ffbbb2028c5d27e340f071eda85
CamilaCarrenoV <camila.carreno89@gmail.com>
2025-01-17 07:48:18 -0300

    Extended Add method to support an unknown number of inputs.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # string-calculator-F-
-# string-calculator-F-
--- StringCalculator.fsx
+++ StringCalculator.fsx
@@ -2,14 +2,10 @@ let add numbers =
     match numbers with
     | "" -> 0
     | _ -> 
-
-    let arrayNumbers = numbers.Split(',')
-    match arrayNumbers with
-    | arr when arr.Length > 2 -> failwith "error"
-    | _ -> 
-        arrayNumbers
-        |> Array.map (fun x -> 
-            match System.Int32.TryParse(x) with
-            | (true, value) -> value 
-            | (false, _) -> 0)     
-        |> Array.sum
+     let arrayNumbers = 
+         numbers.Split(',')
+         |> Array.map (fun x -> 
+             match System.Int32.TryParse(x) with
+             | (true, value) -> value 
+             | (false, _) -> 0)     
+     arrayNumbers |> Array.sum
